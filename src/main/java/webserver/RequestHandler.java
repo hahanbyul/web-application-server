80d37217210946bdef10d29af4da67a4677da3bf
Hanbyul Kim <hahanbyul@gmail.com>
2019-04-11 23:35:22 +0900

    [SOLVE] req. 2

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,10 +9,15 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -41,8 +46,16 @@ public class RequestHandler extends Thread {
         		if (line.startsWith("GET")) {
         			String[] tokens = line.split(" ");
         			url = tokens[1];
-        			
-        			
+        			if (url.startsWith("/user/create")) {
+        				int index = url.indexOf("?");
+        				String requestPath = url.substring(0, index);
+        				String params = url.substring(index + 1);
+        				
+        				Map<String, String> query = HttpRequestUtils.parseQueryString(params);
+        				
+        				User user = new User(query.get("userId"), query.get("password"), query.get("name"), query.get("email"));
+        				System.out.println(user.toString());
+        			}
         		}
         	}
         	while (!"".equals(line));
